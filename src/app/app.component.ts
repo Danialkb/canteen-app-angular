273f5a2171c1f9f1164249ac509e8f0fd35a39b7
Valeria <livaleria25@gmail.com>
2024-10-18 21:58:53 +0500

    nav&details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {UserRegistrationComponent} from "./core/user-registration/user-registration.component";
+import {NavigationBarComponent} from "./core/navigation-bar/navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, UserRegistrationComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, UserRegistrationComponent, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
