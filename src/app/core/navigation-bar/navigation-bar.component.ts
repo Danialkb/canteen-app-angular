3657776c387bc14b614b3a5a80d8f1f2bfa0d96b
Danial <bidaybek044@gmail.com>
2024-10-29 15:39:38 +0500

    refactor navigation br

--- src/app/core/navigation-bar/navigation-bar.component.ts
+++ src/app/core/navigation-bar/navigation-bar.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {FoodListComponent} from "../food-list/food-list.component";
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzMenuModule} from "ng-zorro-antd/menu";
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzDropDownModule} from "ng-zorro-antd/dropdown";
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, NzLayoutModule, NzMenuModule, NzIconModule, NzDropDownModule],
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
